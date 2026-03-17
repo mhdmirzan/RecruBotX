70cbcfc6cd82d2dba63e19a46cd75a3eb28fcb93
Mohammed Mirzan <mirzanfawas@gmail.com>
2026-03-17 16:11:34 +0500

    Update allowed origins for CORS configuration

--- Backend/main.py
+++ Backend/main.py
@@ -35,7 +35,7 @@ app = FastAPI(
 # Configure CORS
 origins = os.getenv(
     "ALLOWED_ORIGINS", 
-    "http://localhost:3000,http://127.0.0.1:3000,https://recrubotx.vercel.app,https://victorious-hill-0116e0f00.4.azurestaticapps.net"
+    "http://localhost:3000,http://127.0.0.1:3000,https://recrubotx.vercel.app,https://green-coast-0ded80d00.4.azurestaticapps.net"
 ).split(",")
 
 app.add_middleware(
